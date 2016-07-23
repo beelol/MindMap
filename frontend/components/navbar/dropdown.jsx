f828f8236b732cb4df0547aace569a8a9ac22a39
Bilal Itani <bilalitani1@gmail.com>
2016-07-23 08:50:22 -0700

    Add working sign out

--- frontend/components/navbar/dropdown.jsx
+++ frontend/components/navbar/dropdown.jsx
@@ -1,11 +1,19 @@
 const React = require('react');
+const SessionActions = require('../../actions/session_actions');
 
 const Dropdown = React.createClass({
+  _handleLogOut(){
+    SessionActions.logOut();
+  },
+
   render () {
     return(
       <div className='dropdown'>
           <ul className="list">
-            <li className='dropdown-item'>Sign Out</li>
+            <li className='dropdown-item'
+                onClick={this._handleLogOut}>Sign Out</li>
+            <li className='dropdown-item'>Profile</li>
+            <li className='dropdown-item'>Settings</li>
           </ul>
       </div>
     );
