4bd95abee7423f9b7d7a73b6ed208bc262574822
Maximilian Rutyna <mrutyna777@gmail.com>
2016-07-25 10:37:12 -0700

    Configure Routes to API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
+    resources :listings
+    resources :boards
+    resources :textboxes
+    resources :photoboxes
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
   end
