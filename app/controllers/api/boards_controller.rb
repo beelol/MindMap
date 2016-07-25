f92adcfbb34b2c030a48de8462b0e6d58ab284e9
Maximilian Rutyna <mrutyna777@gmail.com>
2016-07-25 10:03:13 -0700

    Add Boards Controller

---
+++ app/controllers/api/boards_controller.rb
@@ -0,0 +1,35 @@
+class Api::BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def create
+    @board = Board.create!(board_params)
+
+    render :show
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+
+    @board.update!(board_params)
+
+    render :show
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+
+    @board.destroy
+
+    render :index
+  end
+
+  def board_params
+    params.require(:board).permit(:name, :description, :listing_id, :author_id, :master_board_id, :ord)
+  end
+end
